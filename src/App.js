c01f295a2047f674fb61e5f198d5e55bad30b68b
Kimd0ng <kdongh0406@gmail.com>
2024-07-21 19:51:47 +0900

    :bug: fix : conflict

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
 import MainPage from "./pages/mainPage";
-import Chatting from "./components/Chatting/Chatting";
+import ChattingPage from "./pages/chattingPage";
 import ExplainPage from "./pages/explainPage";
 import LoginPage from "./components/User/LoginPage";
 import SignUpPage from "./components/User/SignUpPage";
-import ChattingPage from "./pages/chattingPage";
+
 
 function App() {
     return (
@@ -13,22 +13,13 @@ function App() {
             <Header/>
             <Routes>
                 <Route path="/" element={<MainPage/>}/>
-                <Route path="/ai" element={<Chatting/>}/>
+                <Route path="/ai" element={<ChattingPage />} />
                 <Route path="/ai/explain" element={<ExplainPage/>}/>
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/signup" element={<SignUpPage/>}/>
             </Routes>
         </BrowserRouter>
     );
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/ai" element={<ChattingPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
 }
 
 export default App;
