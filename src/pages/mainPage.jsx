37565dfcde1ea15d5b2ce61bdf27b5672e035f4a
Kimd0ng <kdongh0406@gmail.com>
2024-07-21 16:06:39 +0900

    :sparkles: feat : mainPage init

---
+++ src/pages/mainPage.jsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import styles from "./mainPage.module.css";
+import Chatting from "../components/Chatting/Chatting";
+
+import Header from "../components/common/Header";
+import Main from "../components/common/Main";
+
+function MainPage() {
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  return (
+    <div className={styles.container}>
+      <Main />
+      <div className={styles.chatButton} onClick={() => setIsExpanded(true)}>
+        ?
+      </div>
+      {isExpanded && (
+        <div className={styles.chatWindow}>
+          <div className={styles.chatHeader}>
+            <span>채팅 상담</span>
+            <button onClick={() => setIsExpanded(false)}>닫기</button>
+          </div>
+          <div className={styles.chatBody}>
+          {/*여기에 이제 채팅 컴포넌트 들어갈 거임 */}
+            <Chatting />
+          </div>
+          <button
+            className={styles.expandButton}
+            onClick={() => {
+              /* 모달 열기 로직 */
+            }}
+          >
+            확장
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default MainPage;
