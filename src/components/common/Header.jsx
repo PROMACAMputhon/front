914251a0b3dcc648db4aceb37e037839928b0b03
Kimd0ng <kdongh0406@gmail.com>
2024-07-21 19:37:54 +0900

    :sparkles: feat : Header frist css

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -1,30 +1,54 @@
 import React from "react";
 import styles from "./Header.module.css";
 import { Bell, Globe, Search, User } from "lucide-react";
+import {useLocation} from "react-router-dom";
+import {ReactComponent as Logo} from "../../assets/icons/logo.svg";
+import {Link} from "react-router-dom";
 
 function Header() {
-  return (
-    <header className={styles.header}>
-      <div className={styles.logo}>
-        <img src="api/placeholder/150/50" alt="Dongguk University Logo" />
-      </div>
-      <nav className={styles.mainNav}>
-        <ul>
-          <li>동국대학교</li>
-          <li>WISE캠퍼스</li>
-          <li>의료원</li>
-          <li>발전기금</li>
-        </ul>
-      </nav>
-      <div className={styles.rightNav}>
-        <Bell size={20} />
-        <Globe size={20} />
-        <User size={20} />
-        <span>로그인</span>
-        <Search size={20} />
-      </div>
-    </header>
-  );
+    const location = useLocation();
+    const isHomePage = location.pathname === "/";
+
+    return (
+        <header className={styles.container}>
+            {isHomePage ? (
+                <div className={styles.header}>
+                    <div className={styles.logoSection}>
+                        <Logo width={150} height={50}/>
+                    </div>
+                    <nav className={styles.mainNav}>
+                        <ul>
+                            <li>동국대학교</li>
+                            <li>WISE캠퍼스</li>
+                            <li>의료원</li>
+                            <li>발전기금</li>
+                        </ul>
+                    </nav>
+                    <div className={styles.rightNav}>
+                        <Bell size={20}/>
+                        <Globe size={20}/>
+                        <User size={20}/>
+                        <span>로그인</span>
+                        <Search size={20}/>
+                    </div>
+                </div>
+            ) : (
+                <nav className={styles.chatHeader}>
+                    <div className={styles.logoSection}>
+                        <Link to={"/"}>
+                            <Logo width={150} height={50}/>
+                        </Link>
+                    </div>
+                    <div className={styles.chatTitle}>
+                        동고동락 캐릭톡
+                    </div>
+                    <div className={styles.chatExplain}>
+                        <Link to={"/ai/explain"}>소개</Link>
+                    </div>
+                </nav>
+            )}
+        </header>
+    );
 }
 
 export default Header;
