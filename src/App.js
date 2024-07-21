aba5bf5c42e6041b97f381c64b86ea98414657ee
kyujenius <hkj020607@gmail.com>
2024-07-21 17:33:01 +0900

    ✨ feat: Recoil 및 Route 초기세팅

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/common/Header";
 import MainPage from "./pages/mainPage";
+import Chatting from "./components/Chatting/Chatting";
 
 function App() {
   return (
-      <BrowserRouter>
-          <Header />
-          <Routes>
-              <Route path="/" element={<MainPage />} />
-              <Route path="/ai" element={<Chatting />} />
-          </Routes>
-      </BrowserRouter>
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/ai" element={<Chatting />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
