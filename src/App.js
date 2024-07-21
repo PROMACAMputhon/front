4a079b80bb9dfabf21b708d88e3c9374e59c548e
Kimd0ng <kdongh0406@gmail.com>
2024-07-21 19:39:03 +0900

    :sparkles: feat : explainPage, login, signup page 라우팅

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Header from "./components/common/Header";
 import MainPage from "./pages/mainPage";
 import Chatting from "./components/Chatting/Chatting";
+import ExplainPage from "./pages/explainPage";
+import LoginPage from "./components/User/LoginPage";
+import SignUpPage from "./components/User/SignUpPage";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/ai" element={<Chatting />} />
-      </Routes>
-    </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <Header/>
+            <Routes>
+                <Route path="/" element={<MainPage/>}/>
+                <Route path="/ai" element={<Chatting/>}/>
+                <Route path="/ai/explain" element={<ExplainPage/>}/>
+                <Route path="/login" element={<LoginPage/>}/>
+                <Route path="/signup" element={<SignUpPage/>}/>
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
 export default App;
